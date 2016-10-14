163212c9d58513b96fefd72ee79abed2ec160e8c
TristanMeinke <tristan.meinke@gmail.com>
2016-10-14 16:00:05 -0700

    Initial commit.
    Bones of HTML and back-end logic built. Minimal styling added.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var numInput;
+var numList = [];
+
+var populateArray = function (numInput) {
+  for (i = 1; i < numInput; i++) {
+    numList.push(i);
+  }
+}
+
+var outputPingPong = function () {
+  numList.map(function(integers) {
+    if (integers === 0) {
+
+    }
+    else if (integers % 15 === 0) {
+
+    }
+    else if (integers % 5 === 0) {
+
+    }
+    else if (integers % 5 === 0) {
+
+    }
+  });
+}
