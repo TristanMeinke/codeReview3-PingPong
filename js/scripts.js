f8f4c673dc2acad75390077693eb26bf509fc96e
TristanMeinke <tristan.meinke@gmail.com>
2016-10-15 17:51:46 -0700

    Final commit before submission.
    All functionality complete.  Readme added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,40 @@
+//Variable Declarations
+
 var numInput;
 var numList = [];
 
+//Back-End Logic
+
 var populateArray = function (numInput) {
-  for (i = 1; i < numInput; i++) {
-    numList.push(i);
+  if (numInput > 10000)
+  {
+    $("#output").append("<li><h4>" + "Please enter a number less than or equal to 10,000." + "</h4></li>");
   }
+  else if (numInput === NaN){
+    $("#output").append("<li><h4>" + "Please enter a number less than or equal to 10,000." + "</h4></li>");
+  }
+  else
+  {
+    for (i = 1; i < numInput + 1; i++)
+    {
+      numList.push(i);
+    }
+  }
+
 }
 
 var outputPingPong = function () {
-  numList.each(function(integer) {
-    if (integer % 15 === 0) {
+  numList.map(function(integer) {
+    if (integer % 15 === 0)
+    {
       $("#output").append("<li>" + "Ping Pong!" + "</li>");
     }
-    else if (integer % 5 === 0) {
+    else if (integer % 5 === 0)
+    {
       $("#output").append("<li>" + "...Pong!" + "</li>");
     }
-    else if (integer % 5 === 0) {
+    else if (integer % 3 === 0)
+    {
       $("#output").append("<li>" + "Ping..." + "</li>");
     }
     else
@@ -24,3 +43,24 @@ var outputPingPong = function () {
     }
   });
 }
+
+//Logic pertaining to User Interface
+
+$(document).ready(function () {
+  $("form#userInput").submit(function(event) {
+
+    event.preventDefault();
+    $("#output").empty();
+
+    numInput = parseInt($("input#userInput").val());
+    numList = [];
+    populateArray(numInput);
+    outputPingPong();
+
+    $("#output").show()
+
+    $("#reset").click(function() {
+      location.reload();
+    });
+  });
+});
